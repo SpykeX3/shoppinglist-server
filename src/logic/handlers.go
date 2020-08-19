44604fd31759879e16ad3c3e4ca1e75a782fd4fe
SpykeX3 <spykex3@gmail.com>
2020-08-20 03:30:40 +0700

    added logic and http handlers to create, get, update and delete lists

--- src/logic/handlers.go
+++ src/logic/handlers.go
@@ -2,15 +2,21 @@ package logic
 
 import (
 	"encoding/json"
+	"github.com/dgrijalva/jwt-go"
 	"io/ioutil"
 	"net/http"
 	"shoppinglist-server/src/utils"
 )
 
-func handleGetList(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
+func getUsername(r *http.Request) string {
 	context := r.Context()
-	username := context.Value("username").(string)
+	claims := context.Value("user").(*jwt.Token).Claims.(jwt.MapClaims)
+	return claims["username"].(string)
+}
+
+func HandleGetList(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	username := getUsername(r)
 	authorized, err := hasAccessToList(username, id)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -40,10 +46,9 @@ type requestListContent struct {
 	Content string `json:"content"`
 }
 
-func handleUpdateList(w http.ResponseWriter, r *http.Request) {
+func HandleUpdateList(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
-	context := r.Context()
-	username := context.Value("username").(string)
+	username := getUsername(r)
 	authorized, err := hasAccessToList(username, id)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -73,4 +78,61 @@ func handleUpdateList(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write(utils.NewWrappedError("internal server error"))
 		return
 	}
+}
+
+type requestNamedList struct {
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+type idResp struct {
+	Id string `json:"id"`
+}
+
+func HandleCreateList(w http.ResponseWriter, r *http.Request) {
+	username := getUsername(r)
+	var reqNList requestNamedList
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write(utils.NewWrappedError("internal server error"))
+		return
+	}
+	err = json.Unmarshal(body, &reqNList)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write(utils.NewWrappedError("invalid request body"))
+		return
+	}
+	id, err := createList(username, reqNList.Name, reqNList.Content)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write(utils.NewWrappedError("internal server error"))
+		return
+	}
+	resp, _ := json.Marshal(idResp{Id: id})
+	_, _ = w.Write(resp)
+}
+
+func HandleDeleteList(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	username := getUsername(r)
+	authorized, err := hasAccessToList(username, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write(utils.NewWrappedError("internal server error"))
+		return
+	}
+	if !authorized {
+		_, _ = w.Write(utils.NewWrappedError("access denied"))
+		return
+	}
+
+	err = deleteList(username, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write(utils.NewWrappedError("internal server error"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	return
 }
--- src/logic/list.go
+++ src/logic/list.go
@@ -1,11 +1,15 @@
 package logic
 
-import "time"
+import (
+	"github.com/segmentio/ksuid"
+	"time"
+)
 
 type list struct {
-	Id          string    `bson:"id" json:"id"`
-	LastChanged time.Time `bson:"last_changed" json:"last_changed"`
-	Content     string    `bson:"content" json:"content"`
+	Id           string    `bson:"id" json:"id"`
+	OriginalName string    `bson:"name"`
+	LastChanged  time.Time `bson:"last_changed" json:"last_changed"`
+	Content      string    `bson:"content" json:"content"`
 }
 
 type listLink struct {
@@ -29,4 +33,34 @@ func hasAccessToList(username, id string) (bool, error) {
 		}
 	}
 	return false, nil
+}
+
+func createList(username, name, content string) (string, error) {
+	id := ksuid.New().String()
+	newList := list{
+		Id:           id,
+		OriginalName: name,
+		LastChanged:  time.Now(),
+		Content:      content,
+	}
+	err := insertListInDB(newList)
+	if err != nil {
+		return "", err
+	}
+	err = addToAccessLists(username, id)
+	if err != nil {
+		return "", err
+	}
+	return id, nil
+}
+
+func deleteList(username, id string) error {
+	return removeFromAccessLists(username, id)
+}
+
+func InitNewUser(username string) error {
+	return insertAccessRecord(accessRecord{
+		Username:       username,
+		AvailableLists: make([]listLink, 0),
+	})
 }
