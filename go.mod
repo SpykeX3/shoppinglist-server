6501573b4ecf23bdf5b29f2847141004fb70db12
SpykeX3 <spykex3@gmail.com>
2020-08-19 16:47:23 +0700

    API prototype

---
+++ go.mod
@@ -0,0 +1,11 @@
+module shoppinglist-server
+
+go 1.14
+
+require (
+	github.com/auth0/go-jwt-middleware v0.0.0-20200810150920-a32d7af194d1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.7.4
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/urfave/negroni v1.0.0
+)
---
+++ src/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func getKey(_ *jwt.Token) (interface{}, error) {
+	return []byte("SECRET_KEY"), nil
+}
+
+func handlerPlaceholder(w http.ResponseWriter, _ *http.Request) {
+	_, _ = w.Write([]byte("Placeholder"))
+}
+
+func readEnv() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8080"
+	}
+	if !strings.HasPrefix(port, ":") {
+		port = ":" + port
+	}
+	return port
+}
+
+func main() {
+	port := readEnv()
+
+	unauthenticatedRouter := mux.NewRouter()
+	// Sign in
+	unauthenticatedRouter.HandleFunc("/v1/user/login", handlerPlaceholder)
+	// Create new account
+	unauthenticatedRouter.HandleFunc("/v1/user/register", handlerPlaceholder)
+
+	authenticatedRouter := mux.NewRouter()
+	// Get list contents
+	authenticatedRouter.Path("/v1/list/id").Methods("GET").HandlerFunc(handlerPlaceholder)
+	// Update list contents
+	authenticatedRouter.Path("/v1/list/id").Methods("POST").HandlerFunc(handlerPlaceholder)
+	// Share a list with another user
+	authenticatedRouter.Path("/v1/list/share").Methods("POST").HandlerFunc(handlerPlaceholder)
+	// Get all shared lists
+	authenticatedRouter.Path("/v1/lists/shared").Methods("GET").HandlerFunc(handlerPlaceholder)
+	// Get all available lists
+	authenticatedRouter.Path("/v1/lists/available").Methods("GET").HandlerFunc(handlerPlaceholder)
+	// Get all notifications
+	authenticatedRouter.Path("/v1/notifications/get").Methods("GET").HandlerFunc(handlerPlaceholder)
+	// Delete notification
+	authenticatedRouter.Path("/v1/notifications/delete").Methods("POST").HandlerFunc(handlerPlaceholder)
+	// Get all requests
+	authenticatedRouter.Path("/v1/requests/get").Methods("GET").HandlerFunc(handlerPlaceholder)
+	// Accept request to share a list
+	authenticatedRouter.Path("/v1/requests/accept").Methods("POST").HandlerFunc(handlerPlaceholder)
+	// Decline request to share a list
+	authenticatedRouter.Path("/v1/requests/decline").Methods("POST").HandlerFunc(handlerPlaceholder)
+
+	authMW := negroni.New()
+	authMW.UseFunc(jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: getKey,
+		SigningMethod:       jwt.SigningMethodHS256,
+	}).HandlerWithNext)
+	authMW.UseHandler(authenticatedRouter)
+
+	outerRouter := mux.NewRouter()
+	outerRouter.PathPrefix("/v1/user/").Handler(unauthenticatedRouter)
+	outerRouter.PathPrefix("/v1/").Handler(authMW)
+
+	mainChain := negroni.New()
+	mainChain.Use(negroni.NewLogger())
+	mainChain.UseHandler(outerRouter)
+
+	err := http.ListenAndServe(port, mainChain)
+	log.Println(err)
+}
